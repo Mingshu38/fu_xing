1c70087e01364f115fd04dacc7ff205e577503f1
Mingshu38 <sonia.joly38@gmail.com>
2025-12-16 14:28:01 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const mobileNav = document.getElementById('mobileNav');
+    const burger = document.querySelector('.burger-menu');
+    mobileNav.classList.toggle('active');
+    burger.classList.toggle('active');
+}
