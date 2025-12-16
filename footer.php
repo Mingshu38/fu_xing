d8441e7a4d8233cc72b30e2102fac8d86b26ba19
Mingshu38 <sonia.joly38@gmail.com>
2025-12-16 15:13:33 +0100

    validation html

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-        <p>&copy; <?php echo date('Y'); ?> Mon Élevage de Chiens. Tous droits réservés.</p>
-        <p>Contact : contact@monelevage.fr | Tél : 01 23 45 67 89</p>
-    </footer>
-
-    <script src="./script.js"></script>
-</body>
-</html>
